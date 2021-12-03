beffa72487976c72fedd947a02cfc85dfa0f2508
Laurens van Coeverden <laurensvanc@hotmail.com>
2021-12-03 10:02:25 +0100

    Commented form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 
 class Title extends React.Component{
   render(){
@@ -11,19 +11,24 @@ class Title extends React.Component{
 }
 
 function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  // const { register, handleSubmit, formState: { errors } } = useForm();
 
   return (
     <div className="App">
       <header className="App-header">
         <Title></Title>
-        <form>
-          <input type="text" placeholder="Student Number"></input>
-          <input type="text" placeholder="First Name"></input>
-          <input type="text" placeholder="Last Name"></input>
-        </form>
       </header>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Title></Title>
+    //     <form>
+    //       <input type="text" placeholder="Student Number"></input>
+    //       <input type="text" placeholder="First Name"></input>
+    //       <input type="text" placeholder="Last Name"></input>
+    //     </form>
+    //   </header>
+    // </div>
   );
 }
 
